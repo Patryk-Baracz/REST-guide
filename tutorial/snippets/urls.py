fade79120cda2660340cc6a63b00cc3b55c3f262
Patryk Baracz <p.baracz1990@gmail.com>
2021-01-23 17:42:36 +0100

    part 2 of the TUTORIAL done

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 
 from django.urls import path
-from snippets.views import snippet_list, snippet_detail
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import snippet_list, snippet_detail
 
 urlpatterns = [
     path('snippets/', snippet_list),
     path('snippets/<int:pk>/', snippet_detail),
     
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
