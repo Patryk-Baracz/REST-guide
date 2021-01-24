3d49c45650eac4551f76a5b4f9f16e912b68abc0
Patryk Baracz <p.baracz1990@gmail.com>
2021-01-24 18:37:46 +0100

    part3 about Class-based Views done

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import snippet_list, snippet_detail
+from .views import SnippetList, SnippetDetail
 
 urlpatterns = [
-    path('snippets/', snippet_list),
-    path('snippets/<int:pk>/', snippet_detail),
+    path('snippets/', SnippetList.as_view()),
+    path('snippets/<int:pk>/', SnippetDetail.as_view()),
     
 ]
 
