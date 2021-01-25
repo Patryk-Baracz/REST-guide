821c62b3959b98bc673b4e9d2cf11fa791b609d7
Patryk Baracz <p.baracz1990@gmail.com>
2021-01-25 10:54:50 +0100

    Tutorial part 5 done

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -25,16 +25,33 @@ from django.contrib.auth.models import User
 
 # to samo poniżej:
 
-class SnippetSerializer(serializers.ModelSerializer):
+# class SnippetSerializer(serializers.ModelSerializer):
+#     owner = serializers.ReadOnlyField(source='owner.username')
+#
+#     class Meta:
+#         model = Snippet
+#         fields = ['id', 'title', 'code', 'lineons', 'language', 'style', 'owner']
+#
+# class UserSerializer(serializers.ModelSerializer):
+#     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+#
+#     class Meta:
+#         model = User
+#         fields = {'id', 'username', 'snippets'}
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'lineons', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'owner', 'title', 'code', 'lineons', 'language', 'style']
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = {'id', 'username', 'snippets'}
+        fields = ['url', 'id', 'username', 'snippets']
--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -16,14 +16,16 @@ Including another URLconf
 
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import SnippetList, SnippetDetail, UserList, UserDetail
+from .views import SnippetList, SnippetDetail, UserList, UserDetail, api_root, SnippetHighlight
 
 urlpatterns = [
-    path('snippets/', SnippetList.as_view()),
-    path('snippets/<int:pk>/', SnippetDetail.as_view()),
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>', UserDetail.as_view()),
-    
+    path('snippets/', SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', SnippetDetail.as_view(), name='snippet-detail'),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>', UserDetail.as_view(), name='user-detail'),
+    path('', api_root),
+    path('snippets/<int:pk>/highlight/', SnippetHighlight.as_view(), name='snippet-highlitgh'),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
